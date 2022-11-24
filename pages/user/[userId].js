ccff343705ef0aa24ab593f91d831c6bba11ca76
itsparasbisht <iamparasbisht@gmail.com>
2022-11-24 20:46:29 +0530

    user details

--- pages/user/[userId].js
+++ pages/user/[userId].js
@@ -1,18 +1,31 @@
 import { useRouter } from "next/router";
 import React from "react";
 
-function User() {
+function User({ user }) {
   const router = useRouter();
-
   const userId = router.query.userId;
 
-  return <div>User - {userId}</div>;
+  console.log(user);
+
+  return (
+    <div>
+      {user && (
+        <div key={user.id}>
+          <h1>{user.name}</h1>
+          <h3>{user.phone}</h3>
+          <p style={{ whiteSpace: "pre-wrap" }}>
+            {JSON.stringify(user.address, null, 4)}
+          </p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default User;
 
 export async function getStaticPaths() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
   const paths = data.map((user) => {
@@ -33,7 +46,7 @@ export async function getStaticProps(context) {
   const { params } = context;
 
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${params.userId}`
+    `https://jsonplaceholder.typicode.com/users/${params.userId}`
   );
   const user = await res.json();
 
