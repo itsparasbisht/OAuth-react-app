1237294a7c48fc54cc8c2f6ddbb6abcc950ec8e4
itsparasbisht <iamparasbisht@gmail.com>
2022-11-26 19:10:36 +0530

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,6 @@
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-export default function Home({ users, failed }) {
-  console.log(users);
-
-  if (failed) {
-    return <h1 style={{ textAlign: "center" }}>Failed to get the users</h1>;
-  }
-
-  return (
-    <div className={styles.container}>
-      {users.length > 0 &&
-        users.map((user) => (
-          <Link href={`/user/${user.id}`} key={user.id}>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                marginTop: "10px",
-              }}
-            >
-              <h1>{user.name}</h1>
-              <h3 style={{ marginLeft: "20px" }}>username - {user.username}</h3>
-            </div>
-          </Link>
-        ))}
-    </div>
-  );
-}
-
-export async function getStaticProps() {
-  try {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
-
-    const users = await response.json();
-
-    return {
-      props: {
-        users,
-      },
-    };
-  } catch (error) {
-    return {
-      props: {
-        failed: true,
-      },
-    };
-  }
+export default function Home() {
+  return <div className={styles.container}>hello</div>;
 }
