6df634aacb39ab4fb58ea8520698647235629593
itsparasbisht <iamparasbisht@gmail.com>
2023-01-02 20:11:25 +0530

    setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "../styles/globals.css";
+
+const clientID = process.env.NEXT_PUBLIC_CLIENT_ID_OAUTH;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GoogleOAuthProvider clientId={clientID}>
+      <Component {...pageProps} />
+    </GoogleOAuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
+import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>hello</div>;
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => console.log(tokenResponse),
+  });
+
+  return (
+    <div className={styles.container}>
+      <h1>Google SignIn</h1>
+      <button onClick={() => login()}>Log In</button>
+    </div>
+  );
 }
