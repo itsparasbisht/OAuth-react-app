b0a5b08131a53d1e4c3e50123dd5573221f7cc05
itsparasbisht <iamparasbisht@gmail.com>
2023-01-02 20:41:22 +0530

    user info

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
 import { GoogleLogin, useGoogleLogin } from "@react-oauth/google";
-import Link from "next/link";
+import jwtDecode from "jwt-decode";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [token, setToken] = useState();
+
+  console.log(token);
+
+  useEffect(() => {
+    if (token) {
+      fetch(
+        `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${token}`
+      ).then((res) => {
+        res.json().then((data) => {
+          console.log(data);
+        });
+      });
+    }
+  }, [token]);
+
   const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => console.log(tokenResponse),
+    onSuccess: (tokenResponse) => {
+      setToken(tokenResponse.access_token);
+    },
   });
 
   return (
