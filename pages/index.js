92e27d0cff78af35a02bca65dfccde508340e5ed
itsparasbisht <iamparasbisht@gmail.com>
2023-01-04 18:57:25 +0530

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 export default function Component() {
   const { data: session } = useSession();
   console.log(session);
   if (session) {
     return (
-      <>
+      <div>
+        <Image src={session.user.image} width={100} height={100} /> <br />
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
-      </>
+      </div>
     );
   }
   return (
